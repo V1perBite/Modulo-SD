bea3f3870f5c79255c4edc2dcb5298440f31c68a
Engel <romerogodoyengel@gmail.com>
2025-03-23 22:56:35 -0500

    segundo incremento se agregaron diseños al login y al home, tambien se modificaron urls.py y views.py

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import *
-from django.contrib.auth.views import LogoutView
-
+from . import views
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('', home_view, name='home'),
+    path('', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- login/views.py
+++ login/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.templatetags.static import static
-
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
@@ -12,19 +12,18 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')  # Asegúrate de que 'home' está en tus URLs
-
+            return redirect('home')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
     else:
         form = AuthenticationForm()
 
     return render(request, 'usuarios/login.html', {'form': form})
 
-
 @login_required
 def home_view(request):
     return render(request, 'index/home.html')
 
-
 def logout_view(request):
     logout(request)
-    return redirect('login') 
+    return redirect('login')

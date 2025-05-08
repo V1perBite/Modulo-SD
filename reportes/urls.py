a5471b68f11c8f6f5a1cf7a6b5592575835430bf
xofranc <santiagop200101@gmail.com>
2025-05-07 23:47:27 -0500

    Add API endpoint for report generation; update URL patterns

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import vista_reportes
+from .views import vista_reportes, ReporteArchivoAPIView
 
 urlpatterns = [
     path('nuevo_formulario/', vista_reportes, name='vista_reportes'),
+    path('api/reportes/', ReporteArchivoAPIView.as_view(), name='api-reportes'),
+
 ]
